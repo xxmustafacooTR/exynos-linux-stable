31e57ccfa5f79189b13d36d3dd52ede52bbeb987
Diep Quynh <remilia.1505@gmail.com>
2020-04-04 13:20:45 +0700

    Revert "sched/fair: use min capacity when evaluating active cpus"
    This reverts commit f964120739fc08339f0e4368a4bad099fdfb4537.
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7117,11 +7117,10 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 			/* Favor CPUs with maximum spare capacity */
 			if ((capacity_orig == target_capacity) &&
-			    ((capacity_orig - min_capped_util) <
-				target_max_spare_cap))
+			    ((capacity_orig - new_util) < target_max_spare_cap))
 				continue;
 
-			target_max_spare_cap = capacity_orig - min_capped_util;
+			target_max_spare_cap = capacity_orig - new_util;
 			target_capacity = capacity_orig;
 			target_util = new_util;
 			target_cpu = i;
