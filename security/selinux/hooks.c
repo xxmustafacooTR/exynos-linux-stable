18b9c59a16e15051011adf70894f052fc6f3a728
rifek4 <dzidekck1234@gmail.com>
2020-05-05 16:05:35 +0200

    selinux: don't require auditing
    Audit is bad. It affects performance under all circumstances. It's also
    dirty.
    
    From LWN: "Andy submitted a patch to fix this particular problem, but he
    didn't stop there. He has come to the conclusion that the audit
    subsystem is beyond repair, so his patch marks the whole thing as being
    broken, making it generally inaccessible. He cited a number of problems
    beyond this security issue: it hurts performance even when it is not
    being used, it is not (in his mind) reliable, it has problems with
    various architectures, and "its approach to freeing memory is
    terrifying." All told, Andy said, we're better off without it"
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    (cherry picked from commit 2af6b94e4a6ec4b82db3a0fb9d9e408134988b26)
    
    selinux: don't require auditing
    
    Audit is bad. It affects performance under all circumstances. It's also
    dirty.
    
    From LWN: "Andy submitted a patch to fix this particular problem, but he
    didn't stop there. He has come to the conclusion that the audit
    subsystem is beyond repair, so his patch marks the whole thing as being
    broken, making it generally inaccessible. He cited a number of problems
    beyond this security issue: it hurts performance even when it is not
    being used, it is not (in his mind) reliable, it has problems with
    various architectures, and "its approach to freeing memory is
    terrifying." All told, Andy said, we're better off without it"
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    
    v
    
    (cherry picked from commit ec3f662b38ed90a9f88941374b5f4d125193aeee)

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3434,6 +3434,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 					   int result,
 					   unsigned flags)
 {
+#ifdef CONFIG_AUDIT
 	struct common_audit_data ad;
 	struct inode_security_struct *isec = inode->i_security;
 	int rc;
@@ -3448,6 +3449,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 			    audited, denied, result, &ad, flags);
 	if (rc)
 		return rc;
+#endif
 	return 0;
 }
 
