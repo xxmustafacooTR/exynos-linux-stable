7aaadc07cc8f11ff4bb67cf8b3a1eb01fd4cbf26
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2022-11-27 22:21:47 +0300

    treewide: fix build with clang

--- arch/arm64/crypto/last_file_asm.c
+++ arch/arm64/crypto/last_file_asm.c
@@ -4,5 +4,9 @@ const unsigned char last_crypto_asm_rodata = 0x20;
 __attribute__ ((section(".text"), unused))
 void last_crypto_asm_text(void){}
 
+#ifdef CC_USE_CLANG
+__attribute__ ((section(".init.text"), unused))
+#else
 __attribute__ ((section(".init.text"), optimize("-O0"), unused))
+#endif
 static void last_crypto_asm_init(void){};
--- crypto/last_file.c
+++ crypto/last_file.c
@@ -4,5 +4,9 @@ const unsigned char last_crypto_rodata = 0x20;
 __attribute__ ((section(".text"), unused))
 void last_crypto_text(void){}
 
+#ifdef CC_USE_CLANG
+__attribute__ ((section(".init.text"), unused))
+#else
 __attribute__ ((section(".init.text"), optimize("-O0"), unused))
+#endif
 static void last_crypto_init(void){};
