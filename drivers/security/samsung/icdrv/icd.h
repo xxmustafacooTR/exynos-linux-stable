f021854431c450f564e044fb88661b68566f7bae
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2022-12-23 16:30:47 +0300

    drivers: security: use N770FXXU8HVK5 ones

--- drivers/security/samsung/icdrv/icd.h
+++ drivers/security/samsung/icdrv/icd.h
@@ -1,8 +1,8 @@
 /*
- * Get/Set OEM flags
  *
  * Copyright (C) 2017 Samsung Electronics, Inc.
  * Jonghun Song, <justin.song@samsung.com>
+ * Nguyen Quy Minh <minh.nq1@samsung.com>
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -14,32 +14,14 @@
  * GNU General Public License for more details.
  */
 
-#ifndef __OEM_FLAG_H
-#define __OEM_FLAG_H
-
-#define OEMFLAG_SUCCESS 0
-#define OEMFLAG_FAIL -1
-
-enum oemflag_id {
-	OEMFLAG_NONE = 0,
-	OEMFLAG_MIN_FLAG = 2,
-	OEMFLAG_TZ_DRM,
-	OEMFLAG_FIDO,
-	OEMFLAG_CC,
-	OEMFLAG_ETC,
-	OEMFLAG_NUM_OF_FLAG,
-};
+#ifndef __ICD_H
+#define __ICD_H
 
 #ifdef CONFIG_KUNIT
 #include <kunit/mock.h>
+#include <linux/file.h>
 #endif
-
 #ifdef CONFIG_KUNIT
-extern int oem_flags_set(enum oemflag_id index);
-extern int oem_flags_get(enum oemflag_id index);
-#else
-int oem_flags_set(enum oemflag_id index);
-int oem_flags_get(enum oemflag_id index);
+extern bool contains_str(const char * const array[], const char *str);
 #endif
-
 #endif
