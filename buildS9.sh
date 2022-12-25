ffb9f7779da3b89ff71bc73a27a7d76c72971af3
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2022-12-25 20:48:03 +0300

    BuildScript: a way for creating kernels without cleaning source

--- buildS9.sh
+++ buildS9.sh
@@ -10,6 +10,8 @@ export ARCH=arm64 && export SUBARCH=arm64
 ZIP_DIR="/home/$USER/Android/Kernel/Zip"
 CUR_DIR=$PWD
 
+rm -rf vmlinux.* drivers/gator_5.27/gator_src_md5.h scripts/dtbtool_exynos/dtbtool arch/arm64/boot/dtb.img arch/arm64/boot/dts/exynos/*dtb* arch/arm64/configs/exynos9810_temp_defconfig
+
 cp -vr $CUR_DIR/arch/arm64/configs/exynos9810_defconfig $CUR_DIR/arch/arm64/configs/exynos9810_temp_defconfig
 echo "" >> $CUR_DIR/arch/arm64/configs/exynos9810_temp_defconfig
 cat $CUR_DIR/arch/arm64/configs/exynos9810-starxlte_defconfig >> $CUR_DIR/arch/arm64/configs/exynos9810_temp_defconfig
