444001c838ff5e02548c8963a50409529b980858
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2023-06-20 01:49:03 +0300

    mali/exynos: add voltage control

--- drivers/gpu/arm/exynos/frontend/gpex_clock.c
+++ drivers/gpu/arm/exynos/frontend/gpex_clock.c
@@ -77,10 +77,7 @@ bool gpex_clock_get_unlock_freqs_status()
 {
 	return clk_info.unlock_freqs;
 }
-/*******************************************
- * static helper functions
- ******************************************/
-static int gpex_clock_update_config_data_from_dt()
+int gpex_clock_update_config_data_from_dt()
 {
 	dt_clock_item *dt_clock_table = gpexbe_devicetree_get_clock_table();
 	int ret = 0;
@@ -124,7 +121,9 @@ static int gpex_clock_update_config_data_from_dt()
 
 	return 0;
 }
-
+/*******************************************
+ * static helper functions
+ ******************************************/
 static int set_clock_using_calapi(int clk)
 {
 	int ret = 0;
