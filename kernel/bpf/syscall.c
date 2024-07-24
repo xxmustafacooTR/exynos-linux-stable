cb8bbe9b599121bcff4c8c54a07ce19131832718
Mustafa Gökmen <mustafa.gokmen2004@gmail.com>
2024-07-24 06:03:36 +0300

    kernel: bpf: syscall: replace __vmalloc_node_flags_caller with __vmalloc_node_flags

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -77,8 +77,7 @@ void *bpf_map_area_alloc(size_t size, int numa_node)
 			return area;
 	}
 
-	return __vmalloc_node_flags_caller(size, numa_node, GFP_KERNEL | flags,
-					   __builtin_return_address(0));
+	return __vmalloc_node_flags(size, numa_node, GFP_KERNEL | flags);
 }
 
 void bpf_map_area_free(void *area)
